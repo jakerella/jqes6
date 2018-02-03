605783d64396550da805a8b38f1f21c3b134393b
Jordan Kasper <jordan@jordankasper.com>
2018-02-03 17:08:01 -0500

    Switch to using ES6 modules (export/import)

--- jqes6.js
+++ jqes6.js
@@ -12,256 +12,253 @@
  *
  **************************************************************************/
 
-(function() {
-    'use strict';
+'use strict';
 
-    let selection = [];
+let selection = [];
 
-    class Collection extends Array {
-        constructor(nodeList = []) {
-            if (Number.isInteger(nodeList)) { nodeList = []; }
-            super(...nodeList.filter((item) => item.tagName));
-        }
-
-        find(selector) {
-            let children = [];
-            this.forEach((node) => {
-                children = [...children, ...select(selector, node)];
-            });
-            return new Collection(children);
-        }
-
-        hide() {
-            this.toggle('hide');
-        }
-
-        show() {
-            this.toggle('show');
-        }
-
-        toggle(force) {
-            this.forEach((node) => {
-                let display = 'none';
-                let show = false;
-                let curr = node.style.display;
-                if (force === 'show' || (curr === 'none' && !force)) {
-                    show = true;
-                }
-
-                if (show) {
-                    let temp = document.createElement(node.tagName);
-                    document.body.appendChild(temp);
-                    display = window.getComputedStyle(temp).display;
-                    document.body.removeChild(temp);
-                }
-                node.style.display = display;
-            });
-            return this;
-        }
-
-        addClass(cls) {
-            return this.toggleClass(cls, true);
-        }
-        removeClass(cls) {
-            return this.toggleClass(cls, false);
-        }
-        toggleClass(cls, state) {
-            let names = cls.split(/\s/);
-            this.forEach((node) => {
-                names.forEach((name) => {
-                    if (state === true) {
-                        node.classList.add(name);
-                    } else if (state === false) {
-                        node.classList.remove(name);
-                    } else if (node.classList.contains(name)) {
-                        node.classList.remove(name);
-                    } else {
-                        node.classList.add(name);
-                    }
-                });
-            });
-            return this;
-        }
-
-        on(evtName, target, fn) {
-            if (typeof(target) === 'function') {
-                fn = target;
-                target = null;
-            }
-            this.forEach((node) => {
-                node[`${evtName}_handler`] = function(evt) {
-                    if (target && !evt.target.matches(target)) { return evt.preventDefault(); }
-                    fn.apply( (target) ? evt.target : node, [...arguments] );
-                };
-                node.addEventListener(evtName, node[`${evtName}_handler`]);
-            });
-            return this;
-        }
-        off(evtName) {
-            this.forEach((node) => node.removeEventListener(evtName, node[`${evtName}_handler`]));
-            return this;
-        }
-
-        html(content) {
-            if (!content) { return this.map((node) => node.innerHTML).join(''); }
-
-            let html = content;
-            if (content.tagName) { html = content.outerHTML; }
-            this.forEach((node) => {
-                node.innerHTML = html;
-            });
-            return this;
-        }
-
-        text(content) {
-            if (!content) { return this.map((node) => node.innerText).join(''); }
-
-            let text = content;
-            if (content.tagName) { text = content.innerText; }
-            this.forEach((node) => {
-                node.innerText = text;
-            });
-            return this;
-        }
-
-        append(content) {
-            let html = content;
-            if (content.tagName) { html = content.outerHTML; }
-            this.forEach((node) => {
-                node.innerHTML += html;
-            });
-            return this;
-        }
-
-        appendTo(selector) {
-            let elem = select(selector);
-            let old = this.map((node) => node);
-            this.length = 0;
-            elem.forEach((parent) => {
-                old.forEach((child) => {
-                    parent.innerHTML += child.outerHTML;
-                });
-                this.push(...[...parent.children].slice(parent.children.length - old.length));
-            });
-            return this;
-        }
-
-        prepend(content) {
-            let html = content;
-            if (content.tagName) { html = content.outerHTML; }
-            this.forEach((node) => {
-                node.innerHTML = html + node.innerHTML;
-            });
-            return this;
-        }
-
-        prependTo(selector) {
-            let elem = select(selector);
-            let old = this.map((node) => node).reverse();
-            this.length = 0;
-            elem.forEach((parent) => {
-                old.forEach((child) => {
-                    parent.innerHTML = child.outerHTML + parent.innerHTML;
-                });
-                this.push(...[...parent.children].slice(0, old.length));
-            });
-            return this;
-        }
-
-        attr(prop, value) {
-            if (typeof(prop) === 'string' && value === undefined) {
-                return this[0] && this[0].getAttribute(prop);
-            } else if (typeof(prop) === 'string' && value) {
-                this.forEach((node) => node.setAttribute(prop, value.toString()));
-            } else if (typeof(prop) === 'string' && !value && value !== 0) {
-                this.forEach((node) => node.removeAttribute(prop));
-            } else if (typeof(prop) === 'object') {
-                Object.keys(prop).forEach((key) => this.attr(key, prop[key]));
-            }
-            return this;
-        }
-    }
-    Collection.prototype.prop = Collection.prototype.attr;
-
-    function select(selector = '', context = document) {
-        let selection;
-
-        if (typeof(context) === 'string') {
-            return select(context).find(selector);
-        } else if (!context || !context.querySelectorAll) {
-            context = document;
-        }
-
-        if (typeof(selector) === 'string') {
-            if (/^</.test(selector)) {
-                let temp = document.createElement('div');
-                temp.innerHTML = selector;
-                selection = [...temp.children];
-                temp = null;
-            } else {
-                try {
-                    selection = [...context.querySelectorAll(selector)];
-                } catch (err) {
-                    selection = [];
-                }
-            }
-        } else if (selector && selector.tagName) {
-            selection = [selector];
-        } else {
-            try { selection = [...selector]; } catch (err) { /* don't care here... leave it empty */ }
-        }
-        return new Collection(selection);
+class Collection extends Array {
+    constructor(nodeList = []) {
+        if (Number.isInteger(nodeList)) { nodeList = []; }
+        super(...nodeList.filter((item) => item.tagName));
     }
 
-    window.$ = select;
-
-    window.$.ajax = function jqFetch(url, settings = {}) {
-        if (typeof(url) === 'object') {
-            settings = url;
-            url = settings.url;
-        }
-
-        if (settings.type && !settings.method) {
-            settings.method = settings.type;
-        }
-        if (settings.data && !settings.body) {
-            if (typeof(settings.data) === 'object') {
-                settings.body = JSON.stringify(settings.data);
-            } else {
-                settings.body = settings.data;
-            }
-        }
-
-        return new Promise((resolve, reject) => {
-            let res = null;
-            window.fetch(url, settings)
-                .then(response => {
-                    res = response;
-                    if (response.status > 399) {
-                        reject(response, 'error', new Error(`${response.status} ${response.statusText}`));
-                    } else if (/\/json( |;|$)/.test(response.headers.get('content-type'))) {
-                        return response.json();
-                    } else if (/^image/.test(response.headers.get('content-type'))) {
-                        return response.blob();
-                    } else {
-                        return response.text();
-                    }
-                })
-                .then(content => {
-                    if (content instanceof Blob) {
-                        try {
-                            resolve(URL.createObjectURL(content), 'success', res);
-                        } catch (error) {
-                            reject(res, 'error', error);
-                        }
-                    } else {
-                        resolve(content, 'success', res);
-                    }
-                })
-                .catch(error => {
-                    reject(res, 'error', error);
-                });
+    find(selector) {
+        let children = [];
+        this.forEach((node) => {
+            children = [...children, ...select(selector, node)];
         });
-    };
+        return new Collection(children);
+    }
 
-}());
+    hide() {
+        this.toggle('hide');
+    }
+
+    show() {
+        this.toggle('show');
+    }
+
+    toggle(force) {
+        this.forEach((node) => {
+            let display = 'none';
+            let show = false;
+            let curr = node.style.display;
+            if (force === 'show' || (curr === 'none' && !force)) {
+                show = true;
+            }
+
+            if (show) {
+                let temp = document.createElement(node.tagName);
+                document.body.appendChild(temp);
+                display = window.getComputedStyle(temp).display;
+                document.body.removeChild(temp);
+            }
+            node.style.display = display;
+        });
+        return this;
+    }
+
+    addClass(cls) {
+        return this.toggleClass(cls, true);
+    }
+    removeClass(cls) {
+        return this.toggleClass(cls, false);
+    }
+    toggleClass(cls, state) {
+        let names = cls.split(/\s/);
+        this.forEach((node) => {
+            names.forEach((name) => {
+                if (state === true) {
+                    node.classList.add(name);
+                } else if (state === false) {
+                    node.classList.remove(name);
+                } else if (node.classList.contains(name)) {
+                    node.classList.remove(name);
+                } else {
+                    node.classList.add(name);
+                }
+            });
+        });
+        return this;
+    }
+
+    on(evtName, target, fn) {
+        if (typeof(target) === 'function') {
+            fn = target;
+            target = null;
+        }
+        this.forEach((node) => {
+            node[`${evtName}_handler`] = function(evt) {
+                if (target && !evt.target.matches(target)) { return evt.preventDefault(); }
+                fn.apply( (target) ? evt.target : node, [...arguments] );
+            };
+            node.addEventListener(evtName, node[`${evtName}_handler`]);
+        });
+        return this;
+    }
+    off(evtName) {
+        this.forEach((node) => node.removeEventListener(evtName, node[`${evtName}_handler`]));
+        return this;
+    }
+
+    html(content) {
+        if (!content) { return this.map((node) => node.innerHTML).join(''); }
+
+        let html = content;
+        if (content.tagName) { html = content.outerHTML; }
+        this.forEach((node) => {
+            node.innerHTML = html;
+        });
+        return this;
+    }
+
+    text(content) {
+        if (!content) { return this.map((node) => node.innerText).join(''); }
+
+        let text = content;
+        if (content.tagName) { text = content.innerText; }
+        this.forEach((node) => {
+            node.innerText = text;
+        });
+        return this;
+    }
+
+    append(content) {
+        let html = content;
+        if (content.tagName) { html = content.outerHTML; }
+        this.forEach((node) => {
+            node.innerHTML += html;
+        });
+        return this;
+    }
+
+    appendTo(selector) {
+        let elem = select(selector);
+        let old = this.map((node) => node);
+        this.length = 0;
+        elem.forEach((parent) => {
+            old.forEach((child) => {
+                parent.innerHTML += child.outerHTML;
+            });
+            this.push(...[...parent.children].slice(parent.children.length - old.length));
+        });
+        return this;
+    }
+
+    prepend(content) {
+        let html = content;
+        if (content.tagName) { html = content.outerHTML; }
+        this.forEach((node) => {
+            node.innerHTML = html + node.innerHTML;
+        });
+        return this;
+    }
+
+    prependTo(selector) {
+        let elem = select(selector);
+        let old = this.map((node) => node).reverse();
+        this.length = 0;
+        elem.forEach((parent) => {
+            old.forEach((child) => {
+                parent.innerHTML = child.outerHTML + parent.innerHTML;
+            });
+            this.push(...[...parent.children].slice(0, old.length));
+        });
+        return this;
+    }
+
+    attr(prop, value) {
+        if (typeof(prop) === 'string' && value === undefined) {
+            return this[0] && this[0].getAttribute(prop);
+        } else if (typeof(prop) === 'string' && value) {
+            this.forEach((node) => node.setAttribute(prop, value.toString()));
+        } else if (typeof(prop) === 'string' && !value && value !== 0) {
+            this.forEach((node) => node.removeAttribute(prop));
+        } else if (typeof(prop) === 'object') {
+            Object.keys(prop).forEach((key) => this.attr(key, prop[key]));
+        }
+        return this;
+    }
+}
+Collection.prototype.prop = Collection.prototype.attr;
+
+const $ = function select(selector = '', context = document) {
+    let selection;
+
+    if (typeof(context) === 'string') {
+        return select(context).find(selector);
+    } else if (!context || !context.querySelectorAll) {
+        context = document;
+    }
+
+    if (typeof(selector) === 'string') {
+        if (/^</.test(selector)) {
+            let temp = document.createElement('div');
+            temp.innerHTML = selector;
+            selection = [...temp.children];
+            temp = null;
+        } else {
+            try {
+                selection = [...context.querySelectorAll(selector)];
+            } catch (err) {
+                selection = [];
+            }
+        }
+    } else if (selector && selector.tagName) {
+        selection = [selector];
+    } else {
+        try { selection = [...selector]; } catch (err) { /* don't care here... leave it empty */ }
+    }
+    return new Collection(selection);
+}
+
+$.ajax = function jqFetch(url, settings = {}) {
+    if (typeof(url) === 'object') {
+        settings = url;
+        url = settings.url;
+    }
+
+    if (settings.type && !settings.method) {
+        settings.method = settings.type;
+    }
+    if (settings.data && !settings.body) {
+        if (typeof(settings.data) === 'object') {
+            settings.body = JSON.stringify(settings.data);
+        } else {
+            settings.body = settings.data;
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        let res = null;
+        window.fetch(url, settings)
+            .then(response => {
+                res = response;
+                if (response.status > 399) {
+                    reject(response, 'error', new Error(`${response.status} ${response.statusText}`));
+                } else if (/\/json( |;|$)/.test(response.headers.get('content-type'))) {
+                    return response.json();
+                } else if (/^image/.test(response.headers.get('content-type'))) {
+                    return response.blob();
+                } else {
+                    return response.text();
+                }
+            })
+            .then(content => {
+                if (content instanceof Blob) {
+                    try {
+                        resolve(URL.createObjectURL(content), 'success', res);
+                    } catch (error) {
+                        reject(res, 'error', error);
+                    }
+                } else {
+                    resolve(content, 'success', res);
+                }
+            })
+            .catch(error => {
+                reject(res, 'error', error);
+            });
+    });
+};
+
+export default $;
--- test/core.js
+++ test/core.js
@@ -1,86 +1,88 @@
 
-(function(q, $) {
+const q = window.QUnit;
+
+if (/HeadlessChrome/.test(window.navigator.userAgent)) {
     q.config.autostart = false;
+}
 
-    const fixture = document.getElementById('qunit-fixture');
+import $ from '../jqes6.js';
+const fixture = document.getElementById('qunit-fixture');
 
-    q.module('Select function', function(hooks) {
+q.module('Select function', function(hooks) {
 
-        q.test('main select function exists', function(assert) {
-            assert.ok(window.$, 'The $ object exists');
-            assert.strictEqual(typeof(window.$), 'function', '$ is a function');
-        });
-
-        q.test('result set is correct type(s)', function(assert) {
-            assert.strictEqual(typeof($()), 'object', 'result is an object');
-            assert.strictEqual($().__proto__.constructor.name, 'Collection', 'result is a Collection');
-            assert.ok($() instanceof Array, 'result is an Array');
-        });
-
-        q.test('bad selector results in empty set', function(assert) {
-            assert.strictEqual($().length, 0, 'undefined selector');
-            assert.strictEqual($(null).length, 0, 'null selector');
-            assert.strictEqual($('').length, 0, 'empty string selector');
-            assert.strictEqual($('asdfghjkl').length, 0, 'made up selector');
-            assert.strictEqual($('dd').length, 0, 'non-existent tag selector');
-            assert.strictEqual($('.foobar').length, 0, 'non-existent class selector');
-            assert.strictEqual($('#foobar').length, 0, 'non-existent id selector');
-        });
-
-        q.test('single element returned with id selector', function(assert) {
-            const results = $('#article');
-            assert.strictEqual(results.length, 1, 'result set length is correct');
-            assert.strictEqual(results[0], document.getElementById('article'), 'result is the article tag');
-        });
-
-        q.test('multiple elements returned with tag selector', function(assert) {
-            const results = $('#list li');
-            assert.strictEqual(results.length, 3, 'result set length is correct');
-            assert.strictEqual(results[0], fixture.getElementsByTagName('li')[0], 'first result is correct');
-            assert.strictEqual(results[1], fixture.getElementsByTagName('li')[1], 'second result is correct');
-            assert.strictEqual(results[2], fixture.getElementsByTagName('li')[2], 'third result is correct');
-        });
-
-        q.test('selecting with node works', function(assert) {
-            const results = $(document.getElementById('article'));
-            assert.strictEqual(results.length, 1, 'result length is correct');
-            assert.strictEqual(results[0], document.getElementById('article'), 'result node is correct');
-        });
-
-        q.test('selecting with array of nodes', function(assert) {
-            const results = $([document.getElementById('article'), document.getElementById('article-paragraph')]);
-            assert.strictEqual(results.length, 2, 'result length is correct');
-            assert.strictEqual(results[0], document.getElementById('article'), 'first result node is correct');
-            assert.strictEqual(results[1], document.getElementById('article-paragraph'), 'second result node is correct');
-        });
-
-        q.test('selecting with nodelist', function(assert) {
-            const results = $(fixture.getElementsByTagName('li'));
-            assert.strictEqual(results.length, 3, 'result length is correct');
-            assert.strictEqual(results[0], fixture.getElementsByTagName('li')[0], 'first result is correct');
-            assert.strictEqual(results[1], fixture.getElementsByTagName('li')[1], 'second result is correct');
-            assert.strictEqual(results[2], fixture.getElementsByTagName('li')[2], 'third result is correct');
-        });
-
-        q.test('selecting with html produces detached element', function(assert) {
-            const result = $('<time class="foobar">the time</time>');
-            assert.strictEqual(typeof(result), 'object', 'result is an object');
-            assert.strictEqual(result.__proto__.constructor.name, 'Collection', 'result is a Collection');
-            assert.strictEqual(result.length, 1, 'result length is correct');
-            assert.strictEqual(result[0].tagName, 'TIME', 'result tagName is correct');
-            assert.strictEqual(result[0].innerText, 'the time', 'result content is correct');
-            assert.strictEqual(result[0].classList.toString(), 'foobar', 'result class list is correct');
-            assert.strictEqual(result[0].parentNode.tagName, 'DIV', 'result parent is correct (div)');
-            assert.strictEqual(result[0].parentNode.parentNode, null, 'result parent of parent is correct (detached)');
-        });
-
-        q.test('selecting with non-node/array object fails with empty result', function(assert) {
-            const results = $({ foo: 'bar'});
-            assert.strictEqual(typeof(results), 'object', 'result is an object');
-            assert.strictEqual(results.__proto__.constructor.name, 'Collection', 'result is a Collection');
-            assert.strictEqual(results.length, 0, 'result length is zero');
-        });
-
+    q.test('main select function exists', function(assert) {
+        assert.ok($, 'The $ object exists');
+        assert.strictEqual(typeof($), 'function', '$ is a function');
     });
 
-})(QUnit, window.$);
+    q.test('result set is correct type(s)', function(assert) {
+        assert.strictEqual(typeof($()), 'object', 'result is an object');
+        assert.strictEqual($().__proto__.constructor.name, 'Collection', 'result is a Collection');
+        assert.ok($() instanceof Array, 'result is an Array');
+    });
+
+    q.test('bad selector results in empty set', function(assert) {
+        assert.strictEqual($().length, 0, 'undefined selector');
+        assert.strictEqual($(null).length, 0, 'null selector');
+        assert.strictEqual($('').length, 0, 'empty string selector');
+        assert.strictEqual($('asdfghjkl').length, 0, 'made up selector');
+        assert.strictEqual($('dd').length, 0, 'non-existent tag selector');
+        assert.strictEqual($('.foobar').length, 0, 'non-existent class selector');
+        assert.strictEqual($('#foobar').length, 0, 'non-existent id selector');
+    });
+
+    q.test('single element returned with id selector', function(assert) {
+        const results = $('#article');
+        assert.strictEqual(results.length, 1, 'result set length is correct');
+        assert.strictEqual(results[0], document.getElementById('article'), 'result is the article tag');
+    });
+
+    q.test('multiple elements returned with tag selector', function(assert) {
+        const results = $('#list li');
+        assert.strictEqual(results.length, 3, 'result set length is correct');
+        assert.strictEqual(results[0], fixture.getElementsByTagName('li')[0], 'first result is correct');
+        assert.strictEqual(results[1], fixture.getElementsByTagName('li')[1], 'second result is correct');
+        assert.strictEqual(results[2], fixture.getElementsByTagName('li')[2], 'third result is correct');
+    });
+
+    q.test('selecting with node works', function(assert) {
+        const results = $(document.getElementById('article'));
+        assert.strictEqual(results.length, 1, 'result length is correct');
+        assert.strictEqual(results[0], document.getElementById('article'), 'result node is correct');
+    });
+
+    q.test('selecting with array of nodes', function(assert) {
+        const results = $([document.getElementById('article'), document.getElementById('article-paragraph')]);
+        assert.strictEqual(results.length, 2, 'result length is correct');
+        assert.strictEqual(results[0], document.getElementById('article'), 'first result node is correct');
+        assert.strictEqual(results[1], document.getElementById('article-paragraph'), 'second result node is correct');
+    });
+
+    q.test('selecting with nodelist', function(assert) {
+        const results = $(fixture.getElementsByTagName('li'));
+        assert.strictEqual(results.length, 3, 'result length is correct');
+        assert.strictEqual(results[0], fixture.getElementsByTagName('li')[0], 'first result is correct');
+        assert.strictEqual(results[1], fixture.getElementsByTagName('li')[1], 'second result is correct');
+        assert.strictEqual(results[2], fixture.getElementsByTagName('li')[2], 'third result is correct');
+    });
+
+    q.test('selecting with html produces detached element', function(assert) {
+        const result = $('<time class="foobar">the time</time>');
+        assert.strictEqual(typeof(result), 'object', 'result is an object');
+        assert.strictEqual(result.__proto__.constructor.name, 'Collection', 'result is a Collection');
+        assert.strictEqual(result.length, 1, 'result length is correct');
+        assert.strictEqual(result[0].tagName, 'TIME', 'result tagName is correct');
+        assert.strictEqual(result[0].innerText, 'the time', 'result content is correct');
+        assert.strictEqual(result[0].classList.toString(), 'foobar', 'result class list is correct');
+        assert.strictEqual(result[0].parentNode.tagName, 'DIV', 'result parent is correct (div)');
+        assert.strictEqual(result[0].parentNode.parentNode, null, 'result parent of parent is correct (detached)');
+    });
+
+    q.test('selecting with non-node/array object fails with empty result', function(assert) {
+        const results = $({ foo: 'bar'});
+        assert.strictEqual(typeof(results), 'object', 'result is an object');
+        assert.strictEqual(results.__proto__.constructor.name, 'Collection', 'result is a Collection');
+        assert.strictEqual(results.length, 0, 'result length is zero');
+    });
+
+});
